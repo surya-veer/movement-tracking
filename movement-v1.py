c0cdbc5c925f2b9437d247ca03dec33bc979137f
Suryaveer <20180313+surya-veer@users.noreply.github.com>
2020-07-25 16:17:27 +0530

    Add files via upload

--- movement-v1.py
+++ movement-v1.py
@@ -28,13 +28,13 @@ def draw_controller(img, cords):
     cv2.circle(img, cords,  size, color['blue'], 2) 
     return [(x1,y1), (x2,y2)]
 
-def keyboard_events(nose_cords, cords, img):
+def keyboard_events(nose_cords, cords, cmd):
     try:
         [(x1,y1), (x2,y2)] = cords
         xc, yc = nose_cords
-    except: 
-        return 
-    cmd = None
+    except Exception as e: 
+        print(e)
+        return
     if xc < x1:
         cmd = "left"
     elif(xc > x2):
@@ -42,22 +42,20 @@ def keyboard_events(nose_cords, cords, img):
     elif(yc<y1):
         cmd = "up"
     elif(yc > y2):
-        
         cmd = "down"
     if cmd:
-        print(cmd)
+        print("Detected movement: ", cmd,  "\n")
         keyboard.press_and_release(cmd)
-        cv2.putText(img, cmd, (10,50), cv2.FONT_HERSHEY_SIMPLEX, 3, color['red'], 1, cv2.LINE_AA)
-    return img
-def reset_press_flag(nose_cords, cords):
+    return img,cmd
+def reset_press_flag(nose_cords, cords,cmd):
     try:
         [(x1,y1), (x2,y2)] = cords
         xc, yc = nose_cords
     except: 
-        return True 
+        return True,cmd
     if x1<xc<x2 and y1<yc<y2:
-        return True
-    return False
+        return True,None
+    return False,cmd
     
 # Loading classifiers
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -69,18 +67,23 @@ video_capture = cv2.VideoCapture(-1)
 width  = video_capture.get(3) # float
 height = video_capture.get(4) # float
 press_flag = False
+cmd = ""
 
 while True:
     # Reading image from video stream
     _, img = video_capture.read()
     img = cv2.flip( img, 1 )
-    # detect nose
+
+    # detect nose and draw
     img, nose_cords = detect_nose(img, faceCascade)
+    cv2.putText(img, cmd, (10,50), cv2.FONT_HERSHEY_SIMPLEX, 1, color['red'], 1, cv2.LINE_AA)
+
+    #draw boundary circle
     cords = draw_controller(img, (int(width/2), int(height//2)) )
-    if press_flag:
-        img = keyboard_events(nose_cords,cords, img)
-        press_flag = False
-    press_flag = reset_press_flag(nose_cords,cords)
+    if press_flag and len(nose_cords):
+        img,cmd = keyboard_events(nose_cords,cords, cmd)
+    press_flag,cmd = reset_press_flag(nose_cords,cords,cmd)
+
     # Writing processed image in a new window
     cv2.imshow("face detection", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
